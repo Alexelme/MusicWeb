6a4331d3c5a13a3cdc69bfe9de848f2302a8f62b
Alexelme <alexelme@users.noreply.github.com>
2026-07-09 23:09:48 +0800

    Fix Netlify deployment paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MusicWeb/',
+  base: './',
   publicDir: 'img',
   plugins: [react()],
 });
